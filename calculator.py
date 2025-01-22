ce43b11958557d65c8b1ee739288160807019d0f
Abubaker Khilji <53000495+Abubaker-khilji@users.noreply.github.com>
2025-01-22 22:41:48 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+# a= 50
+# b=3
+
+# print("the value" ,a ," +",b,"is" ,a+b )
+# print("the value" ,a ," -",b,"is" ,a-b )
+# print("the value" ,a ," *",b,"is" ,a*b )
+# print("the value" ,a ," /",b,"is" ,a/b )
+
+a = input("enter you first number")
+b = input("enter you second number")
+
+print(int(a) + int(b))
+print(int(a) - int(b))
+print(int(a) * int(b))
+print(int(a) / int(b))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
